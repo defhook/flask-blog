6668cb744de241b1de9edd2cfeedc4923672cbd3
lniwn <lniwn@live.com>
2017-04-27 15:29:07 +0800

    [fix] fix cdn path

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ PUBLIC_CDN_DOMAIN = 'cdn.bootcss.com'  # 公用js文件的cdn地址
 SESSION_PROTECTION = 'strong'  # strong basic None
 
 # flask-cdn
-CDN_DOMAIN = 'lniwn.oschina.io/flask-blog/app/'
+CDN_DOMAIN = 'lniwn.oschina.io/flask-blog/app'
 CDN_ENDPOINTS = ['static', 'blog.static']
 CDN_HTTPS = False
 
